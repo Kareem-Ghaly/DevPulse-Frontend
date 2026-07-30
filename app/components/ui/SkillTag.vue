a9e8ddf5d63285de6e833933410dc8d625d391d3
Kareem-Ghaly <km2236833@gmail.com>
2026-07-30 03:21:53 +0300

    Make Skills Base

---
+++ app/components/ui/SkillTag.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+interface Props {
+  skill: string
+}
+
+const props = defineProps<Props>()
+
+const classes = computed(() => getSkillClasses(props.skill))
+</script>
+
+<template>
+  <span class="px-3 py-1.5 rounded-lg text-xs font-semibold border" :class="classes">
+    {{ skill }}
+  </span>
+</template>
